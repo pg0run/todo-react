77c814e66d21f5dc957b1c55a763d1645f77ab1d
pgorun <pgorun@ya.ru>
2017-10-12 23:00:03 +0300

    todo app

---
+++ app/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import KanbanBoardContainer from './components/KanbanBoardContainer';
+
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import KanbanBoard from './components/KanbanBoard';
+import EditCard from './components/EditCard';
+import NewCard from './components/NewCard';
+
+
+
+ReactDOM.render((
+  <Router history={createBrowserHistory()}>
+    <Route component={KanbanBoardContainer}>
+      <Route path="/" component={KanbanBoard}>
+        <Route path="new" component={NewCard} />
+        <Route path="edit/:card_id" component={EditCard} />
+      </Route>
+    </Route>
+  </Router>
+), document.getElementById('root'));
